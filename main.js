15fc3a09b068ab864c449800fa6a515cd99d04fd
Jacob <jakeboone@aol.com>
2014-09-08 14:43:25 -0400

    basic arrow key movement

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
 $(document).on('ready', function() {
   
-// $('.fa-fighter-jet').on(keydown)
+
+
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        $('.fa-fighter-jet').animate({left: "-=10px"}, 'fast');
+        console.log('left')
+        break;
+
+        case 38: // up
+        $('.fa-fighter-jet').animate({top: "-=10px"}, 'fast');
+        console.log('up')
+        break;
+
+        case 39: // right
+        console.log('right')
+		$('.fa-fighter-jet').animate({left: "+=10px"}, 'fast');
+        break;
+
+        case 40:// down
+        $('.fa-fighter-jet').animate({top: "+=10px"}, 'fast');
+        console.log('down') 
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// $( ".fa-fighter-jet" ).keydown(function() {
+//   alert( "Handler for .keypress() called." );
+// });
+
+
 
 
 
