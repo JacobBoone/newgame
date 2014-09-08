039a010b48de57e696924ec4b2c632d8137c4998
Jacob <jakeboone@aol.com>
2014-09-08 18:10:11 -0400

    changed to xwing

--- main.js
+++ main.js
@@ -34,53 +34,53 @@ $(document).on('ready', function() {
 $(document).keydown(function(e) {
     switch(e.which) {
         case 37: // left
-        $('.jet').animate({left: "-=10px"}, 'fast');
+        $('.xwing').animate({left: "-=20px"}, 'fast');
         console.log('left')
         break;
 
         case 38: // up
-        $('.jet').animate({top: "-=10px"}, 'fast');
+        $('.xwing').animate({top: "-=20px"}, 'fast');
         console.log('up')
         break;
 
         case 39: // right
         console.log('right')
-		$('.jet').animate({left: "+=10px"}, 'fast');
+		$('.xwing').animate({left: "+=20px"}, 'fast');
         break;
 
         case 40:// down
-        $('.jet').animate({top: "+=10px"}, 'fast');
+        $('.xwing').animate({top: "+=20px"}, 'fast');
         console.log('down') 
         break;
 
         // ----------WASD
         case 65: // left
-        $('.jet').animate({left: "-=10px"}, 'fast');
+        $('.xwing').animate({left: "-=10px"}, 'fast');
         console.log('left')
         break;
 
         case 87: // up
-        $('.jet').animate({top: "-=10px"}, 'fast');
+        $('.xwing').animate({top: "-=10px"}, 'fast');
         console.log('up')
         break;
 
         case 68: // right
         console.log('right')
-		$('.jet').animate({left: "+=10px"}, 'fast');
+		$('.xwing').animate({left: "+=10px"}, 'fast');
         break;
 
         case 83:// down
-        $('.jet').animate({top: "+=10px"}, 'fast');
+        $('.xwing').animate({top: "+=10px"}, 'fast');
         console.log('down') 
         break;
 
         case 81:// diagonal up left
-        $('.jet').animate({top: "-=10px", left:"-=10px"}, 'fast');
+        $('.xwing').animate({top: "-=10px", left:"-=10px"}, 'fast');
         console.log('diag left up') 
         break;
 
         case 69:// diagonal up right
-        $('.jet').animate({top: "-=10px", left:"+=10px"}, 'fast');
+        $('.xwing').animate({top: "-=10px", left:"+=10px"}, 'fast');
         console.log('diag left up') 
         break;
 
@@ -89,7 +89,6 @@ $(document).keydown(function(e) {
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });
-
 
 
 
